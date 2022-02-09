4706ad9f81c53b0df12e1f0c815daaa0f6707356
bhuvi1724 <79234626+bhuvi1724@users.noreply.github.com>
2022-02-08 18:04:59 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+song1="";
+song2="";
+song="";
+leftwristx="";
+rightwristy="";
+leftwristy="";
+rightwristx="";
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.hide();
+
+    canvas=createCanvas(550,450);
+    canvas.center();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded()
+{
+    console.log("posenet is initilized");
+}
+function preload()
+{
+    song1=loadsound("music.mp3");
+    song2=loadsound("music2.mp3");
+
+}
+function draw()
+{
+    image(video,0,0,550,450);
+}
+function play()
+{
+    song1.play();
+    song2.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function gotPoses(results)
+{
+if (results.length >0) {
+    console.log(results);
+    leftwristx=results[0].pose.leftWrist.x;
+    leftwristy=results[0].pose.leftWrist.y;
+    console.log("leftwristx= "+leftwristx+ "leftwristy= "+leftwristy);
+    rightwristx=results[0].pose.rightWrist.x;
+    rightwristy=results[0].pose.rightWrist.y;
+    console.log("rightwristx= "+rightwristx+ "rightwristy= "+rightwristy);
+
+}
+}
+    
+
+
+   
+
+
+
